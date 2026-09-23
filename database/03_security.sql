b444427e5b5e6f0784d27606f09494dd3dcf8238
John Patrick M. Asuncion <johnpatrickasuncion1@gmail.com>
2026-09-23 18:45:01 +0800

    adding local placeholder in seed.sql

--- database/03_security.sql
+++ database/03_security.sql
@@ -6,11 +6,11 @@ CREATE ROLE IF NOT EXISTS 'librasys_report_role';
 CREATE ROLE IF NOT EXISTS 'librasys_demo_grantor_role';
 
 CREATE USER IF NOT EXISTS 'librasys_api'@'localhost'
-  IDENTIFIED BY 'REPLACE_WITH_LOCAL_SECRET';
+  IDENTIFIED BY 'LibraApi_Local_2026!';
 CREATE USER IF NOT EXISTS 'librasys_report'@'localhost'
-  IDENTIFIED BY 'REPLACE_WITH_LOCAL_SECRET';
+  IDENTIFIED BY 'LibraReport_Local_2026!';
 CREATE USER IF NOT EXISTS 'librasys_demo_grantor'@'localhost'
-  IDENTIFIED BY 'REPLACE_WITH_LOCAL_SECRET';
+  IDENTIFIED BY 'LibraGrant_Local_2026';
 
 GRANT SELECT, INSERT, UPDATE ON librasys.books TO 'librasys_api_role';
 GRANT SELECT, INSERT, UPDATE ON librasys.book_copies TO 'librasys_api_role';
